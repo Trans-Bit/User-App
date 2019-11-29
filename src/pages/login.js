e210bd640afcfbc59e03811ce7f0a053c2e3df8b
Tanisha <tanisha031199@gmail.com>
2019-11-29 20:20:57 +0530

    added

--- src/pages/login.js
+++ src/pages/login.js
@@ -30,29 +30,32 @@ export default class Login extends Component {
   }
   render() {
     return (
-      <div className="container">
-        <h2 className="heading">LOGIN</h2>
-        <input
-          type="text"
-          placeholder="email"
-          onChange={e => this.setState({ email: e.target.value })}
-        />
-        <br />
-        <input
-          type="password"
-          placeholder="password"
-          onChange={e => this.setState({ password: e.target.value })}
-        />
-        <br />
-        <button onClick={e => this.handleSubmit(e)}>LOGIN</button>
-        <br />
-        <div className="text">
-          Not a member?{" "}
-          <Link to="/signup">
-            <span classname="details" style={{ color: "#6f64a3" }}>
-              Sign up
-            </span>
-          </Link>
+      <div>
+        <div className="msg">Message displayed here</div>
+        <div className="container">
+          <h2 className="heading">LOGIN</h2>
+          <input
+            type="text"
+            placeholder="email"
+            onChange={e => this.setState({ email: e.target.value })}
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="password"
+            onChange={e => this.setState({ password: e.target.value })}
+          />
+          <br />
+          <button onClick={e => this.handleSubmit(e)}>LOGIN</button>
+          <br />
+          <div className="text">
+            Not a member?{" "}
+            <Link to="/signup">
+              <span classname="details" style={{ color: "#6f64a3" }}>
+                Sign up
+              </span>
+            </Link>
+          </div>
         </div>
       </div>
     );
